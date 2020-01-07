0d7d6430c8e23645c334b9e382130c361c5db992
EC2 Default User <ec2-user@ip-172-31-32-44.ec2.internal>
2020-01-07 13:10:22 +0000

    added code to js files, aadded css styles to html

---
+++ js/myScript.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("h2").css("text-decoration", "underline"); //undrlines all <h2> elements 
+    $("ul").css("bordr", "solid 1px #ccc"); // adds border to all lists
+    
+});
